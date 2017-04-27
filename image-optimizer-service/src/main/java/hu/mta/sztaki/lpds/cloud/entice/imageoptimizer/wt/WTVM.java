f7825ea28d3a8073b0621daf2e440d6cdfe968ea
Akos Hajnal <ahajnal@sztaki.hu>
2017-04-27 09:56:00 +0200

    Resource leaks, exceptions, status mapping, logs improvements

--- image-optimizer-service/src/main/java/hu/mta/sztaki/lpds/cloud/entice/imageoptimizer/wt/WTVM.java
+++ image-optimizer-service/src/main/java/hu/mta/sztaki/lpds/cloud/entice/imageoptimizer/wt/WTVM.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import javax.ws.rs.core.MediaType;
 import javax.xml.datatype.DatatypeConfigurationException;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
@@ -27,22 +28,20 @@ import com.sun.jersey.multipart.file.FileDataBodyPart;
 import hu.mta.sztaki.lpds.cloud.entice.imageoptimizer.VM;
 import hu.mta.sztaki.lpds.cloud.entice.imageoptimizer.utils.ResourceUtils;
 
-// creates orchestrator VM
+// creates an image optimizer orchestrator VM in WT VMware
 public class WTVM extends VM {
+	
 	private static final Logger log = LoggerFactory.getLogger(WTVM.class);
 	public static final String CLOUD_INTERFACE = "wt";
-	
-	private String userData; // cloud-init
-	
-	private String serverName; 
-	
+
 	// user-defined required parameters (must be present in request json)
 	private final String endpoint;
 	private final String username;
 	private final String password;
 	
 	// VM status 
-	private String vmId = null; // ID of the created server
+	private String vmId = null;
+	private String vmName = null; 
 	private String privateDnsName;
 	private String status = UNKNOWN;
 	
@@ -51,9 +50,8 @@ public class WTVM extends VM {
 		private final String endpoint;
 		private final String username;
 		private final String password;
-		
 		public Builder(String endpoint, String username, String password)  {
-			this.endpoint = endpoint;
+			this.endpoint = endpoint.endsWith("/") ? endpoint : endpoint + "/"; ;
 			this.username = username;
 			this.password = password;
 		}
@@ -61,89 +59,164 @@ public class WTVM extends VM {
 			return new WTVM(this);
 		}
 	}
-	
-	private WTVM(Builder builder) throws MalformedURLException, DatatypeConfigurationException, IOException {
+	private WTVM(Builder builder) {
+		endpoint = builder.endpoint;
 		username = builder.username;
 		password = builder.password;
-		endpoint = builder.endpoint;
-	}
-	
-	private void listVMs() throws Exception {
-		Client client = Client.create();
-		String service = endpoint + (endpoint.endsWith("/") ? "" : "/") + "vms";
-		log.debug("Sending GET '" + service + "'");
-		WebResource webResource = client.resource(service);
-		
-		// send GET
-		ClientResponse response = webResource
-				.header("Authorization", "Basic " + ResourceUtils.base64Encode(username + ":" + password))
-				.get(ClientResponse.class);
-		
-		if (response.getStatus() != 200) {
-			log.error("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
-			throw new Exception("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
-		}
-		
-		String responseString = response.getEntity(String.class);
-		log.info("VMs: " + responseString);
 	}
 	
 	@Override public void run(Map<String, String> parameters) throws Exception {
-		log.info("Launching optimizer VM in FCO...");
-		
-		// parameters for emulating cloud-init
-		if (parameters != null) {
-			userData = parameters.get(USER_DATA);
-		}
-		
-		// run
-		Client client = Client.create();
-		String service = endpoint + (endpoint.endsWith("/") ? "" : "/") + "vms/orquestrator/deploy";
-		log.debug("Sending POST '" + service + "'");
-		WebResource webResource = client.resource(service);
-		
-		// write user data to temp file
-		File fileToUpload = File.createTempFile("user-data", "tmp");
-		PrintWriter out = new PrintWriter(fileToUpload);
-		out.println(userData);
-		out.close();
-		
-		// create multipart body
-		FileDataBodyPart fileDataBodyPart = new FileDataBodyPart("file", fileToUpload, MediaType.APPLICATION_OCTET_STREAM_TYPE);
-        fileDataBodyPart.setContentDisposition(FormDataContentDisposition.name("file").fileName(fileToUpload.getName()).build());
-		MultiPart formData = new FormDataMultiPart();
-		formData.bodyPart(fileDataBodyPart);
+		log.info("Launching VM in VMware...");
+		Client client = null;
+		try {
+			String service = endpoint + "vms/orquestrator/deploy";
+			log.debug("Sending POST to '" + service + "'");
+			
+			// write user-data to a temp file
+			File fileToUpload;
+			try {
+				fileToUpload = File.createTempFile("user-data", "tmp");
+				PrintWriter out = new PrintWriter(fileToUpload);
+				String userData = parameters.get(USER_DATA);
+				if (userData != null) out.println(userData);
+				out.close();
+			} catch (IOException x) {
+				throw new Exception("Cannot create temp file for user-data");
+			}
+			
+			// create multi-part body
+			FileDataBodyPart fileDataBodyPart = new FileDataBodyPart("file", fileToUpload, MediaType.APPLICATION_OCTET_STREAM_TYPE);
+	        fileDataBodyPart.setContentDisposition(FormDataContentDisposition.name("file").fileName(fileToUpload.getName()).build());
+			MultiPart formData = new FormDataMultiPart();
+			formData.bodyPart(fileDataBodyPart);
+	
+			// send POST
+			client = Client.create();
+			WebResource webResource = client.resource(service);
+			ClientResponse response = webResource
+					.header("Authorization", "Basic " + ResourceUtils.base64Encode(username + ":" + password))
+					.type(MediaType.MULTIPART_FORM_DATA)
+					.accept(MediaType.APPLICATION_JSON)
+					.post(ClientResponse.class, formData);
+			
+			// cleanup
+			try { formData.close(); } catch (IOException x) {} // silently ignore 
+			fileToUpload.delete();
+			
+			if (response.getStatus() != 200) {
+				log.error("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
+				throw new Exception("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
+			}
+			
+			String responseString = response.getEntity(String.class);
+			log.debug("VM created: " + responseString);
+			
+			JSONObject responseJSON = null;
+	    	try { responseJSON = new JSONObject(new JSONTokener(responseString)); }
+	    	catch (JSONException e) { 
+				log.warn("Invalid JSON: " + e.getMessage());
+				throw new Exception("Invalid JSON: " + e.getMessage());
+	    	}
+			
+			String message = responseJSON.optString("message");
+			log.debug("message: " + message);
+	    	String status = responseJSON.optString("status");
+	    	this.status = mapVMStatus(status);
+			this.vmName = responseJSON.optString("name");
+			log.info("status: " + this.status);
+			log.info("name: " + this.vmName);
 
-		// send POST
-		ClientResponse response = webResource
-				.header("Authorization", "Basic " + ResourceUtils.base64Encode(username + ":" + password))
-				.type(MediaType.MULTIPART_FORM_DATA)
-				.accept(MediaType.APPLICATION_JSON)
-				.post(ClientResponse.class, formData);
-		
-		formData.close();
-		
-		if (response.getStatus() != 200) {
-			log.error("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
-			throw new Exception("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
+		} finally {
+			if (client != null) client.destroy();
 		}
+	}
+
+	@Override public void describeInstance() throws Exception {
+		log.debug("Describe instance: " + vmId);
+		if (vmId == null) lookupVMId();
+		if (vmId == null) {	log.debug("vmid not yet available"); return; }
 		
-		String responseString = response.getEntity(String.class);
-		log.info("VM created: " + responseString);
-		JSONObject responseJSON = null;
-    	try { responseJSON = new JSONObject(new JSONTokener(responseString)); }
-    	catch (JSONException e) { 
-			log.warn("Invalid JSON: " + e.getMessage());
-			throw new Exception("Invalid JSON: " + e.getMessage());
-    	}
-		String status = responseJSON.optString("status");
-		serverName = responseJSON.optString("name");
-		String message = responseJSON.optString("message");
-		log.info("status: " + status);
-		log.info("name: " + serverName);
-		log.info("message: " + message);
-		
-//		this.vmId = responseString;
+		Client client = null;
+		try {
+			String service = endpoint + "vms/" + vmId;
+			log.debug("Sending GET to '" + service + "'");
+			client = Client.create();
+			WebResource webResource = client.resource(service);
+			ClientResponse response = webResource				
+					.header("Authorization", "Basic " + ResourceUtils.base64Encode(username + ":" + password))
+					.get(ClientResponse.class);
+			if (response.getStatus() != 200) {
+				log.error("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
+				throw new Exception("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
+			} 
+			// process response
+			String responseString = response.getEntity(String.class);
+			log.debug("Processing response (status 200): " + responseString);
+			JSONObject responseJSON = null;
+	    	try { responseJSON = new JSONObject(new JSONTokener(responseString)); }
+	    	catch (JSONException e) { 
+				log.error("Invalid JSON object: " + e.getMessage());
+				throw new Exception("Invalid JSON object: " + e.getMessage());
+	    	}
+			
+			this.status = mapVMStatus(responseJSON.optString("status"));
+			this.privateDnsName = responseJSON.optString("address");
+			
+			log.info("Instance name: " + this.vmName);
+			log.info("Instance id: " + this.vmId);
+			log.info("IP: " + this.privateDnsName);
+			log.info("Status: " + this.status);
+		} finally {
+			if (client != null) client.destroy();
+		}
+	}	
+
+	private void lookupVMId() throws Exception {
+		if (vmId != null) {	log.debug("vmid already set"); return; }
+		log.debug("Looking VM id by VM name: " + this.vmName);
+		Client client = null;
+		try {
+			String service = endpoint + "vms";
+			log.debug("Sending GET to '" + service + "'");
+			client = Client.create();
+			WebResource webResource = client.resource(service);
+			ClientResponse response = webResource
+					.header("Authorization", "Basic " + ResourceUtils.base64Encode(username + ":" + password))
+					.get(ClientResponse.class);
+			if (response.getStatus() != 200) {
+				log.error("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
+				throw new Exception("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
+			}
+			// returns json array of objects
+			String responseString = response.getEntity(String.class);
+			log.debug("VMs: " + responseString);
+			JSONArray jsonArray;
+	    	try { jsonArray = new JSONArray(new JSONTokener(responseString)); }
+	    	catch (JSONException e) { 
+				log.error("Invalid JSON array: " + e.getMessage());
+				throw new Exception("Invalid JSON object: " + e.getMessage());
+	    	}
+	    	for (int i = 0; i < jsonArray.length(); i++) {
+		    	try { 
+		    		JSONObject record = jsonArray.getJSONObject(i);
+		    		String name = record.optString("name");
+		    		String id = record.optString("vmid");
+		    		if (this.vmName != null && this.vmName.equals(name)) {
+		    			log.debug("VM name found in VMs listing");
+		    			if (id != null) {
+		    				this.vmId = id;
+		    				log.info("vmid: " + this.vmId);
+		    			}
+		    			break;
+		    		}
+		    	} catch (JSONException e) { 
+					log.error("Invalid JSON: object " + e.getMessage());
+					throw new Exception("Invalid JSON object: " + e.getMessage());
+		    	}
+	    	}
+		} finally {
+			if (client != null) client.destroy();
+		}
 	}
 	
 	// getters and setters
@@ -151,136 +224,91 @@ public class WTVM extends VM {
 	@Override public String getStatus() { return status; }
 	@Override public String getIP() { return privateDnsName; }
 
-	@Override public void describeInstance() throws Exception {
-		log.debug("Describe server: " + vmId);
-		if (vmId == null) return;
-		Client client = Client.create();
-		String service = endpoint + (endpoint.endsWith("/") ? "" : "/") + "vms/" + vmId;
-		log.debug("Sending GET '" + service + "'");
-		WebResource webResource = client.resource(service);
-		ClientResponse response = webResource				
-				.header("Authorization", "Basic " + ResourceUtils.base64Encode(username + ":" + password))
-				.get(ClientResponse.class);
-		if (response.getStatus() != 200) {
-			log.error("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
-			throw new Exception("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
-		} 
-		// process response
-		String responseString = response.getEntity(String.class);
-		log.debug("Processing response (status 200): " + responseString);
-		JSONObject responseJSON = null;
-    	try { responseJSON = new JSONObject(new JSONTokener(responseString)); }
-    	catch (JSONException e) { 
-			log.error("Invalid JSON: " + e.getMessage());
-			throw new Exception("Invalid JSON: " + e.getMessage());
-    	}
-		
-		status = mapVMStatus(responseJSON.optString("status"));
-		privateDnsName = responseJSON.optString("address");
-		
-		log.debug("IP: " + privateDnsName);
-		log.debug("Status: " + status);
-	}	
-
 	private String mapVMStatus(String serverStatus) {
-		if (serverStatus == null) return VM.UNKNOWN;
-		// TODO
-//		switch (serverStatus) {
-//			case STARTING:
-//			case MIGRATING:
-//			case REBOOTING:
-//			case RECOVERY:
-//			case BUILDING:
-//			case INSTALLING:
-//				return VM.PENDING;
-//			case RUNNING:
-//				return VM.RUNNING;
-//			case DELETING:
-//				return VM.STOPPING;
-//			case ERROR:
-//				return VM.ERROR;
-//			case STOPPED:
-//				return VM.TERMINATED;
-//			case STOPPING:
-//				return VM.STOPPING;
-//			default:
-//				return VM.UNKNOWN;
-//		}
-		return serverStatus;
+		// FIXME
+		if (VM.RUNNING.equalsIgnoreCase(serverStatus)) return VM.RUNNING;
+		else if (VM.PENDING.equalsIgnoreCase(serverStatus)) return VM.PENDING;
+		else if (VM.STOPPING.equalsIgnoreCase(serverStatus)) return VM.STOPPING;
+		else if (VM.STOPPING.equalsIgnoreCase(serverStatus)) return VM.STOPPING;
+		else if (VM.TERMINATED.equalsIgnoreCase(serverStatus)) return VM.TERMINATED;
+		else if (VM.ERROR.equalsIgnoreCase(serverStatus)) return VM.ERROR;
+		return VM.UNKNOWN;
 	}
 	
 	@Override public void terminate() throws Exception {
-		log.debug("Delete server: " + vmId);
-		if (vmId == null) throw new Exception("Server UUID is null");
-		
-		Client client = Client.create();
-		String service = endpoint + (endpoint.endsWith("/") ? "" : "/") + "vms/" + vmId;
-		log.debug("Sending DELETE '" + service + "'");
-		WebResource webResource = client.resource(service);
-		
-		// send POST
-		ClientResponse response = webResource
-				.header("Authorization", "Basic " + ResourceUtils.base64Encode(username + ":" + password))
-				.delete(ClientResponse.class);
-		
-		
-		if (response.getStatus() != 200) {
-			log.error("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
-			throw new Exception("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
+		log.info("Delete VM: " + vmId);
+		if (vmId == null) throw new Exception("Instance id is null");
+		Client client = null;
+		try {
+			String service = endpoint + "vms/" + vmId;
+			log.debug("Sending DELETE to '" + service + "'");
+			client = Client.create();
+			WebResource webResource = client.resource(service);
+			// send POST
+			ClientResponse response = webResource
+					.header("Authorization", "Basic " + ResourceUtils.base64Encode(username + ":" + password))
+					.delete(ClientResponse.class);
+			if (response.getStatus() != 200) {
+				log.error("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
+				throw new Exception("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
+			}
+			String responseString = response.getEntity(String.class);
+			log.info("VM terminated: " + responseString);
+		} finally {
+			if (client != null) client.destroy();
 		}
-		
-		String responseString = response.getEntity(String.class);
-		log.info("VM terminated: " + responseString);
-		
 		discard();
 	}
 	
 	@Override public void reboot() throws Exception {
-		log.info("Reboot server: " + vmId);
+		log.info("Rebooting VM: " + vmId);
 		if (vmId == null) throw new Exception("VM is null");
-		
-		Client client = Client.create();
-		String service = endpoint + (endpoint.endsWith("/") ? "" : "/") + "vms/" + vmId + "/reboot";
-		log.debug("Sending PUT '" + service + "'");
-		WebResource webResource = client.resource(service);
-		
-		// send PUT
-		ClientResponse response = webResource
-				.header("Authorization", "Basic " + ResourceUtils.base64Encode(username + ":" + password))
-				.put(ClientResponse.class);
-		
-		if (response.getStatus() != 200) {
-			log.error("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
-			throw new Exception("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
+		Client client = null;
+		try {
+			String service = endpoint + (endpoint.endsWith("/") ? "" : "/") + "vms/" + vmId + "/reboot";
+			log.debug("Sending PUT to '" + service + "'");
+			client = Client.create();
+			WebResource webResource = client.resource(service);
+			// send PUT
+			ClientResponse response = webResource
+					.header("Authorization", "Basic " + ResourceUtils.base64Encode(username + ":" + password))
+					.put(ClientResponse.class);
+			if (response.getStatus() != 200) {
+				log.error("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
+				throw new Exception("WT API " + service + " returned HTTP error code: " + response.getStatus() + " " + response.getEntity(String.class));
+			}
+			String responseString = response.getEntity(String.class);
+			log.info("VM rebooted: " + responseString);
+		} finally {
+			if (client != null) client.destroy();
 		}
-		
-		String responseString = response.getEntity(String.class);
-		log.info("VM rebooted: " + responseString);
 	}
 	
 	@Override public void discard() {
-		log.debug("Discarding VM: " + serverName + ", server UUID: " + vmId);
+		log.debug("Discarding VM: " + vmName + ", id: " + vmId);
+		this.vmId = null;
+		this.vmName = null;
+		this.privateDnsName = null;
+		this.privateDnsName = null;
 	}
 	
 	public static void main(String [] args) throws Exception {
-		String endpoint = "http://entice-api.wtelecom.es";
+		String endpoint = "http://api-endpoint";
 		String username = "user";
 		String password = "password";
-		
 		WTVM vm = new WTVM.Builder(endpoint, username, password).build();
 		Map <String, String> params = new HashMap<String, String>();
 		params.put(USER_DATA, ResourceUtils.base64Decode("I2Nsb3VkLWNvbmZpZw0Kd3JpdGVfZmlsZXM6DQotIHBhdGg6IC9yb290L29wdGltaXplLnNoDQogIHBlcm1pc3Npb25zOiAiMDcwMCINCiAgY29udGVudDogfA0KICAgICMhL2Jpbi9iYXNoDQpydW5jbWQ6DQotIGVjaG8gJ0hlbGxvIHdvcmxkIScgPiAvcm9vdC9oZWxsby13b3JsZC50eHQ="));
-//		vm.run(params);
-		
-		vm.listVMs();
-		vm.vmId = "474";
-		vm.describeInstance();
+		vm.run(params);
+//		vm.lookupVMId();
+//		vm.vmId = "474";
+//		vm.describeInstance();
 //		vm.terminate();
-//		do {
-//			Thread.sleep(10000);
-//			log.debug("Polling VM status...");
-//			if (vm.vmId != null) try { vm.describeInstance(); } catch (Exception x) { log.debug(x.getMessage()); }
-//		} while (!vm.status.equals(VM.RUNNING));
-//		vm.terminate();
+		do {
+			Thread.sleep(10000);
+			log.debug("Polling VM status...");
+			if (vm.vmId != null) try { vm.describeInstance(); } catch (Exception x) { log.debug(x.getMessage()); }
+		} while (!vm.status.equals(VM.RUNNING));
+		vm.terminate();
 	}
 }
