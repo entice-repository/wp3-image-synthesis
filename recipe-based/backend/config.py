a5699fbf1c7395425ca0f100177a291d49f41e6e
Attila Csaba Marosi <atisu@sztaki.mta.hu>
2017-06-09 14:44:25 +0200

    backend: update configuration
    - remove not used variables.
    - add description for configuration options.

--- recipe-based/backend/config.py
+++ recipe-based/backend/config.py
@@ -7,46 +7,61 @@ import logging
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfiguration(object):
+    """
+    Base configuration. All others are derived from here.
+    """
+    # Debugging enabled (disable for production site).
     DEBUG = False
+    # Testing enabled (disable for production site).
     TESTING = False
-    APP_PORT = 5000
-    SECURITY_PASSWORD_HASH = ""
-    SECURITY_PASSWORD_SALT = ""
-    SECRET_KEY = ""
+    # Root folder for storing incoming build requests. Must be the same as for the frontend.
     DATADIR = "/tmp/entice-builder/datadir"
+    # Folder for the builder module (working directory). Must be the same as for the frontend.
     SCRIPTDIR = "/tmp/entice-builder/scriptdir"
+    # logging information.
     LOGLEVEL = logging.INFO
     LOGNAME = "entice-ib-backend"
+    # Sleep interval between.
     SCANWAITINTERVAL = 5
+    # Maximum number of parallel running jobs.
     MAX_RUNNING_JOBS = 4
-    
+
 class DebugConfiguration(BaseConfiguration):
+    """
+    Configuration for debugging the application.
+    """
     DEBUG = True
-    APP_PORT = 4000
-    SECURITY_PASSWORD_HASH = "pbkdf2_sha512"
-    SECURITY_PASSWORD_SALT = "CHANGE_ME"
-    SECRET_KEY = "CHANGE_ME"
     LOGLEVEL = logging.DEBUG
     DATADIR = "/tmp/datadir"
     SCRIPTDIR = "/tmp/scriptdir"
     LOGNAME = "entice-ib-backend"
     SCANWAITINTERVAL = 5
     MAX_RUNNING_JOBS = 2
-    
+
+
 class TestConfiguration(BaseConfiguration):
+    """
+    Configuration for testing environment.
+    """
     TESTING = True
-    
+
+
 class LiveConfiguration(BaseConfiguration):
+    """
+    Live configuration.
+
+    Tries to load from a .json configuration file. If it is not found then values defined
+    in BaseConfiguration are used. This is to separate live configuration values from this
+    file. Most of the time setting values in BaseConfiguration instead of here should be
+    fine.
+    """
     sys.stderr.write("Loading config from JSON file.\n")
     try:
-        config_file = "/etc/entice/imagesynthesis-frontend.json"
+        config_file = "/etc/entice/imagesynthesis-backend.json"
         with open(config_file, 'r') as content_file:
             content = content_file.read()
             config_json = json.loads(content)
-        SECURITY_PASSWORD_HASH = "pbkdf2_sha512"
-        SECURITY_PASSWORD_SALT = config_json["imagesynthesis-frontend"].get("password_salt")
-        SECRET_KEY = config_json["imagesynthesis-frontend"].get("secret_key")
-        
+
     except Exception, e:
         sys.stderr.write("WARNING: Could not read configuration from LIVE config file:" + str(e) + "\n")
         pass # DIRRRRRTY HACK!
